0a3fa94ee8f837c15737c535a37bb3474270aacd
prraaddeepp <80219572+prraaddeepp@users.noreply.github.com>
2023-03-10 00:22:09 -0500

    Add files via upload

---
+++ Unit_6/Unit_6/AppDelegate.swift
@@ -0,0 +1,51 @@
+
+
+import UIKit
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ParseSwift.initialize(applicationId: "WUlBgtmNtVUxwCOC6aGfx0NjBGNg52kgqGenvFeI",
+                              clientKey: "X8UcQHEVqbMyATYCIWBnWDUmd0gFhP7UvydXxAdN",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        return true
+    }
+
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+struct GameScore: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    // All custom properties must be optional.
+    var playerName: String?
+    var points: Int?
+}
+
+
+extension GameScore {
+    init(playerName: String, points: Int) {
+        self.playerName = playerName
+        self.points = points
+    }
+}
+
---
+++ Unit_6/Unit_6/Feed/PostCell.swift
@@ -0,0 +1,71 @@
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var postImageView: UIImageView!
+    @IBOutlet private weak var captionLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var blurView: UIVisualEffectView!
+
+    private var imageDataRequest: DataRequest?
+
+    func configure(with post: Post) {
+        if let currentUser = User.current,
+           let lastPostedDate = currentUser.lastPostedDate,
+           let postCreatedDate = post.createdAt,
+           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+
+            blurView.isHidden = abs(diffHours) < 24
+        } else {
+
+            // Default to blur if we can't get or compute the date's above for some reason.
+            blurView.isHidden = false
+        }
+        // Username
+        if let user = post.user {
+            usernameLabel.text = user.username
+        }
+
+        // Image
+        if let imageFile = post.imageFile,
+           let imageUrl = imageFile.url {
+
+            // Use AlamofireImage helper to fetch remote image from URL
+            imageDataRequest = AF.request(imageUrl).responseImage { [weak self] response in
+                switch response.result {
+                case .success(let image):
+                    // Set image view image with fetched image
+                    self?.postImageView.image = image
+                case .failure(let error):
+                    print("❌ Error fetching image: \(error.localizedDescription)")
+                    break
+                }
+            }
+        }
+
+        // Caption
+        captionLabel.text = post.caption
+
+        // Date
+        if let date = post.createdAt {
+            dateLabel.text = DateFormatter.postFormatter.string(from: date)
+        }
+
+    
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // TODO: Pt 1 - Cancel image data request
+
+        // Reset image view image.
+        postImageView.image = nil
+
+        // Cancel image request.
+        imageDataRequest?.cancel()
+    }
+}
